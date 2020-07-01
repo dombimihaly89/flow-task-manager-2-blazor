1c9f181d865073b9fc6de7a938f8dc48400b9f07
Dombi Mihály <dombimihaly89@gmail.com>
2020-07-01 15:17:07 +0200

    Added two new classes into the Models folder in the Shared project.

---
+++ FlowTaskManager.Web/Shared/Models/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTaskManager.Web.Shared.Models
+{
+    public class Task
+    {
+        private int Id { get; set; }
+        private Type Type { get; set; }
+        private string Title { get; set; }
+        private string Content { get; set; }
+        private Difficulty Difficulty { get; set; }
+        private DateTime CreatedAt { get; set; }
+        private DateTime UpdatedAt { get; set; }
+        private int UserId { get; set; }
+        private User User { get; set; }
+    }
+
+    public enum Type
+    {
+        Java,
+        Javascript,
+        Spring,
+        Angular,
+        Linux,
+        Database
+    }
+
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+}
---
+++ FlowTaskManager.Web/Shared/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowTaskManager.Web.Shared.Models
+{
+    public class User
+    {
+        private long Id { get; set; }
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private DateTime DateOfBirth { get; set; }
+        private Role Role { get; set; }
+    }
+    public enum Role
+    {
+        Student,
+        Mentor
+    }
+}
