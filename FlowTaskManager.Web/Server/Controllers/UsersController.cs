5a23d58601d481c5cec64fbca633674320ab80a6
Dombi Mihály <dombimihaly89@gmail.com>
2020-07-03 13:07:25 +0200

    Created a UsersController and implemented GetUsers method in it.

---
+++ FlowTaskManager.Web/Server/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlowTaskManager.Web.Server.Services;
+using FlowTaskManager.Web.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowTaskManager.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetUsers()
+        {
+            try
+            {
+                return Ok(await userService.GetUsers());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+        }
+    }
+}
