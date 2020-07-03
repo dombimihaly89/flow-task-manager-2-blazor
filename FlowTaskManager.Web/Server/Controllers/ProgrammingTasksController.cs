a78f8eb525854fb2cead1a044af8fd1300b72781
dombimihaly89 <dombimihaly89@gmail.com>
2020-07-03 18:58:16 +0200

    Added ProgrammingTasksController into the Server app.

---
+++ FlowTaskManager.Web/Server/Controllers/ProgrammingTasksController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlowTaskManager.Web.Server.Services;
+using FlowTaskManager.Web.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowTaskManager.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingTasksController : ControllerBase
+    {
+        private readonly IProgrammingTaskService programmingTaskService;
+
+        public ProgrammingTasksController(IProgrammingTaskService programmingTaskService)
+        {
+            this.programmingTaskService = programmingTaskService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProgrammingTask>> GetProgrammingTasks()
+        {
+            return await programmingTaskService.GetProgrammingTasks();
+        }
+    }
+}
