6d114c1c7677b122ff928c40c7782e95b7f6d3e9
dombimihaly89 <dombimihaly89@gmail.com>
2020-07-06 17:14:46 +0200

    Installed System.ComponentModel.Annotations to the Web.Shared and added different attributes to the properties due to validation.

--- FlowTaskManager.Web/Shared/Models/ProgrammingTask.cs
+++ FlowTaskManager.Web/Shared/Models/ProgrammingTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace FlowTaskManager.Web.Shared.Models
@@ -7,12 +8,27 @@ namespace FlowTaskManager.Web.Shared.Models
     public class ProgrammingTask
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Type must be provided.")]
         public Type Type { get; set; }
+
+        [Required(ErrorMessage = "Title must be provided.")]
+        [MinLength(3)]
+        [MaxLength(50)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content must be provided.")]
+        [MinLength(10)]
+        [MaxLength(1000)] 
         public string Content { get; set; }
+
+        [Required(ErrorMessage = "Difficulty must be provided.")]
         public Difficulty Difficulty { get; set; }
+
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
+
+        [Required]
         public int UserId { get; set; }
 
         public User User { get; set; }
